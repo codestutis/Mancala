9ebe0ba0b7cc4d1361bf8911375d4cee81a6cbeb
kgb <kgb1043@usnh.edu>
2024-12-20 14:40:34 -0500

    create outline. define functions, add javadocs, etc...

--- Main.java
+++ Main.java
@@ -2,4 +2,11 @@ public class Main {
     public static void main(String[] args){
         
     }
+
+    /**
+     * @return index of which thingy the player wants to move
+    */
+    public static int getMove(){
+        return 0;
+    }
 }
--- Mancala.java
+++ Mancala.java
@@ -1,3 +1,41 @@
 public class Mancala {
-    
+    // first half is computers side second half is players side
+    private int[] board = {4,4,4,4,4,4,0,4,4,4,4,4,4,0};
+
+    public Mancala(){
+
+    }
+
+    /**
+     * prints board with correct formatting
+     */
+    public void printBoard(){
+        System.out.println(board);
+    }
+
+    /**
+     * 
+     * @param index index of the players move
+     * @return true if its a valid move
+     */
+    public boolean isValid(int index){
+        return true;
+    }
+
+    /**
+     * 
+     * @param index index of players move
+     * @return altered board after move was performed
+     */
+    public int[] makeMove(int index){
+        return board;
+    }
+
+    /**
+     * 
+     * @return index of computer generated move
+     */
+    public int AIMove(){
+        return 0;
+    }
 }

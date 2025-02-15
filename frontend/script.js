5ebf8da1f5bd25b53fc40c142044802d9f2f5d9c
codestutis <kestutisbiskis@gmail.com>
2025-02-14 20:12:15 -0500

    LETS GOOO

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,11 @@
 const API_URL = "http://localhost:8080/game";
 let first;
 let last;
+
 async function fetchGameState() {
     const response = await fetch(`${API_URL}/state`);
     const gameState = await response.json();
-    let move = response.currentPlayer % 2 == 1;
+    let move = gameState.currentPlayer % 2 == 1;
     first = move ? 7 : 0;
     last = move ? 12 : 5;
     renderBoard(gameState.board);
@@ -26,20 +27,23 @@ function renderBoard(board) {
         well.innerText = board[well.getAttribute("index")];
     });
 }
+
 document.addEventListener('DOMContentLoaded', function() { 
     // Add event listeners to each well
     fetchGameState();
     document.querySelectorAll('.well').forEach(function(well) {
         well.addEventListener('click', function() {
-            
-            if (well.innerText != 0 && well.getAttribute("index") >= first && well.getAttribute("index") >= first) {
-                makeMove(well.getAttribute("index"));
-                fetchGameState();
+            const index = parseInt(well.getAttribute("index"));
+            if (parseInt(well.innerText) != 0 && index >= first && index <= last) {
+                makeMove(index);
                 console.log("Move accepted");
+            } else {
+                console.log("Invalid move");
             }
         });
     });
 });
+
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
 }
